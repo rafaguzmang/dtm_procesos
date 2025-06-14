284212079512c3c502e6f98821651927e38535f6
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2025-06-14 11:54:01 -0600

    Se repara calculadora de lista de materiales del modelo dtm_odt.

--- controllers/seguimiento_controller.py
+++ controllers/seguimiento_controller.py
@@ -9,3 +9,16 @@ class ProcesosController(http.Controller):
         ordenes = request.env['dtm.proceso'].sudo().search([('status','!=','calidad'),('status','!=','terminado'),('status','!=','instalacion')])
         result = [{'orden':orden.ot_number,'status':orden.status,'tipo':orden.tipe_order,'cliente':orden.name_client,'producto':orden.product_name,'fecha_ini':orden.create_date,'po':orden.po_number,'fecha_rel':orden.date_rel} for orden in ordenes]
         return result
+
+    @http.route('/seguimiento_materiales', type='http', auth='public')
+    def lista_materiales(self, **kwargs):
+        ordenes = request.env['dtm.proceso'].sudo().search(
+            [('status', '!=', 'calidad'), ('status', '!=', 'terminado'), ('status', '!=', 'instalacion')])
+        result = [
+            {'orden': orden.ot_number, 'status': orden.status, 'tipo': orden.tipe_order, 'cliente': orden.name_client,
+             'producto': orden.product_name, 'fecha_ini': orden.create_date, 'po': orden.po_number,
+             'fecha_rel': orden.date_rel} for orden in ordenes]
+        return request.make_response(
+        json.dumps(result, default=str),
+        headers=[('Content-Type', 'application/json')]
+    )
