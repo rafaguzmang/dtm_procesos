811cd970f07ec4667c03d759577d897b3e2238bb
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2025-06-17 10:10:01 -0600

    Se agrega la lista de materiales a la tabla de seguimiento

--- controllers/seguimiento_controller.py
+++ controllers/seguimiento_controller.py
@@ -7,18 +7,39 @@ class ProcesosController(http.Controller):
     @http.route('/seguimiento_procesos', type='json', auth='public')
     def ordenes_trabajo(self, **kwargs):
         ordenes = request.env['dtm.proceso'].sudo().search([('status','!=','calidad'),('status','!=','terminado'),('status','!=','instalacion')])
-        result = [{'orden':orden.ot_number,'status':orden.status,'tipo':orden.tipe_order,'cliente':orden.name_client,'producto':orden.product_name,'fecha_ini':orden.create_date,'po':orden.po_number,'fecha_rel':orden.date_rel} for orden in ordenes]
+        result = [{
+                    'orden':orden.ot_number,
+                    'revision_ot':orden.revision_ot,
+                    'status':orden.status,
+                    'tipo':orden.tipe_order,
+                    'cliente':orden.name_client,
+                    'producto':orden.product_name,
+                    'fecha_ini':orden.create_date,
+                    'po':orden.po_number,
+                    'fecha_rel':orden.date_rel
+                } for orden in ordenes]
         return result
 
-    @http.route('/seguimiento_materiales', type='http', auth='public')
+    @http.route('/seguimiento_materiales', type='json', auth='public')
     def lista_materiales(self, **kwargs):
         ordenes = request.env['dtm.proceso'].sudo().search(
             [('status', '!=', 'calidad'), ('status', '!=', 'terminado'), ('status', '!=', 'instalacion')])
-        result = [
-            {'orden': orden.ot_number, 'status': orden.status, 'tipo': orden.tipe_order, 'cliente': orden.name_client,
-             'producto': orden.product_name, 'fecha_ini': orden.create_date, 'po': orden.po_number,
-             'fecha_rel': orden.date_rel} for orden in ordenes]
-        return request.make_response(
-        json.dumps(result, default=str),
-        headers=[('Content-Type', 'application/json')]
-    )
+
+        result = []
+
+        for orden in ordenes:
+
+            ot_id = request.env['dtm.odt'].sudo().search([('ot_number','=',orden.ot_number),('revision_ot','=',orden.revision_ot)])
+            lista_materiales = [f"{material.materials_list.id}: {material.nombre} {material.medida} - R - {material.materials_required} - {'compras' if material.revision else ''}" for material in ot_id.materials_ids]
+            lista_ordenada = sorted(lista_materiales,key=lambda item: 'compras' in item)
+            result.append({
+                'orden':orden.ot_number,
+                'version':orden.revision_ot,
+                'ot_id':ot_id.id,
+                'materiales':lista_ordenada
+            })
+
+        return result
+        # return request.make_response(
+        # json.dumps(result, default=str),
+        # headers=[('Content-Type', 'application/json')])
--- static/src/js/seguimiento.js
+++ static/src/js/seguimiento.js
@@ -11,7 +11,7 @@ export class Seguimiento extends Component {
         this.state = useState({
             tabla:[],
             total:0,
-            materiales:["item1","item2","item3","item4"]
+            materiales:[]
         })
 
         onWillStart(async() => {
@@ -37,6 +37,20 @@ export class Seguimiento extends Component {
 
      async dataFetch() {
 
+            const response_materiales = await fetch('/seguimiento_materiales',{
+                method:'POST',
+                headers:{
+                    'Content-Type': 'application/json',
+                    'X-Requested-With': 'XMLHttpRequest'
+                },
+                body:JSON.stringify({})
+            })
+
+            const materiales_data = await response_materiales.json();
+
+            console.log('Materiales',materiales_data.result);
+
+
             const response = await fetch('/seguimiento_procesos',{
                         method:'POST',
                         headers: {
@@ -46,10 +60,13 @@ export class Seguimiento extends Component {
                         body:JSON.stringify({})
             })
 
+
+
             const data = await response.json();
             this.state.tabla = data.result;
             this.state.tabla = this.state.tabla.map(row =>(
                 {
+
                     ...row,
                     dias: (((new Date(row.fecha_rel) - new Date().getTime()) / (1000 * 60 * 60 * 24)) + 1).toFixed(0),
                     status: row.status === 'corte'?'Corte':
@@ -62,8 +79,13 @@ export class Seguimiento extends Component {
                             row.status === 'calidad'?'Calidad':
                             row.status === 'instalacion'?'Instalación':
                             row.status === ''?'':null,
+                    materiales_lista: materiales_data.result.filter(item => item.orden === row.orden).flatMap(item => item.materiales.map((mat, index) => ({
+                    id: index + 1,
+                    material: mat
+                }))),
                 }
             ));
+//            console.log(this.state.tabla)
             this.state.tabla.sort((a,b)=> a.dias - b.dias)
             this.state.total = this.state.tabla.length
             console.log('Respuesta:', this.state.tabla);
